a4c0e534f94aac6179f0c744978cbd05c2d22838
Jacky Liu <Jacky35liu@gmail.com>
2022-05-19 17:18:41 -0500

    init JWT folder struct

---
+++ client/src/actions/Auth.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Redux action creators for register & login
+
+
+export default function Auth() {
+  return (
+    <div>Auth</div>
+  )
+}
---
+++ client/src/actions/Message.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// Redux action creators fror resposne message from server
+
+export default function Message() {
+  return (
+    <div>Message</div>
+  )
+}
