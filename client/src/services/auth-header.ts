c994f37cc930069e26a096c5dc94c8be98797a9a
Jacky Liu <Jacky35liu@gmail.com>
2022-05-19 18:16:48 -0500

    add auth header

---
+++ client/src/services/auth-header.ts
@@ -0,0 +1,12 @@
+// We have methods for retrieving data from server
+// In case we access protected resources, the HTTP request needs Authorization header
+
+export default function authHeader() {
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  if (user && user.accessToken) {
+    return { Authorization: 'Bearer ' + user.accessToken };
+  }
+  else {
+    return {};
+  }
+}
