9c52f291795e540b25f72a8dc93cd9c36f4f065f
Jacky Liu <Jacky35liu@gmail.com>
2022-05-20 13:07:41 -0500

    add auth actions

--- client/src/actions/auth.ts
+++ client/src/actions/auth.ts
@@ -8,3 +8,84 @@ import {
   LOGOUT,
   SET_MESSAGE,
 } from "./types";
+
+import authService from "../services/auth.service";
+
+import { SubmitHandler } from "react-hook-form";
+
+interface IUserRegister {
+  firstName: String;
+  lastName: String;
+  email: String;
+  password: String;
+  roles: String;
+  active: boolean;
+}
+
+interface IUserLogin {
+  email: String;
+  password: String;
+}
+
+export const register: SubmitHandler<IUserRegister> = data => (dispatch: any) => {
+  return authService.register(data).then(
+    (res: any) => {
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data.message,
+      });
+      return Promise.resolve();
+    },
+    (err: any) => {
+      const message =
+        (err.response &&
+          err.response.data &&
+          err.response.data.message) ||
+        err.message ||
+        err.toString();
+      dispatch({
+        type: REGISTER_FAIL,
+      });
+      dispatch({
+        type: SET_MESSAGE,
+        payload: message,
+      });
+      return Promise.reject();
+    }
+  );
+};
+
+export const login: SubmitHandler<IUserLogin> = data => (dispatch: any) => {
+  return authService.login(data).then(
+    (res: any) => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res,
+      });
+      return Promise.resolve();
+    },
+    (err: any) => {
+      const message =
+        (err.response &&
+          err.response.data &&
+          err.response.data.message) ||
+        err.message ||
+        err.toString();
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+      dispatch({
+        type: SET_MESSAGE,
+        payload: message,
+      });
+      return Promise.reject();
+    }
+  );
+};
+
+export const logout = () => (dispatch: any) => {
+  authService.logout();
+  dispatch({
+    type: LOGOUT,
+  });
+};
--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
 
 const API_URL = "http://localhost:8080/api/auth/";
 
