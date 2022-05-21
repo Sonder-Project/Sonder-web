7bb7d223fda2ea055a8e3116a5a65920da3b83f0
Jacky Liu <Jacky35liu@gmail.com>
2022-05-21 17:37:18 -0500

    add some stuff to events page

--- client/src/Pages/Events.tsx
+++ client/src/Pages/Events.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Container, Text } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import userService from '../services/user.service';
 
 function Events() {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    userService.getPublicContent()
+      .then(response => {
+        setContent(response.data);
+      },
+        (error) => {
+          const _content =
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString();
+          setContent(_content);
+        }
+      );
+  }, []);
+
   return (
-    <div>Events</div>
+    <Container
+      zIndex={2}
+      position="relative"
+      maxW="container.page"
+      py={{ base: 20, md: 28 }}>
+      <Text>Events</Text>
+      <Text>{content}</Text>
+    </Container>
   )
 }
 
